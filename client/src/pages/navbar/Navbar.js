aab864d4778f95996d2d45e2676d15b57df4b71b
Min Ko <minpko@gmail.com>
2019-11-09 14:50:57 -0500

    Add Navbar component to pages folder

--- client/src/pages/navbar/Navbar.js
+++ client/src/pages/navbar/Navbar.js
@@ -5,13 +5,50 @@ import style from './navbar.module.scss';
 
 // *************************** NAVBAR COMPONENT *************************** //
 const Navbar = () => {
+
+  const logoContainer = (
+    <div className={style.logoContainer}>
+      <h1 className={style.logoTitle}>Boutique Hipster</h1>
+      <h2 className={style.logoSubtitle}>Johnsonville</h2>
+    </div>
+  );
+
+  const links = (
+    <ul className={style.links}>
+      <li>
+        <Link to='/' className={style.link}>Men's</Link>
+      </li>
+      <li>
+        <Link to='/' className={style.link}>Women's</Link>
+      </li>
+      <li>
+        <Link to='/' className={style.link}>Exclusives</Link>
+      </li>
+      {
+        logoContainer
+      }
+      <li>
+        <Link to='/' className={style.link}>Forum</Link>
+      </li>
+      <li>
+        <Link to='/' className={style.link}>Account</Link>
+      </li>
+      <li>
+        <Link to='/' className={style.link}>Cart (0)</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className={style.navbar}>
-      
-      <div className={style.logoContainer}>
-        <h1 className={style.logoTitle}>Boutique Hipster</h1>
-        <h2 className={style.logoSubtitle}>Johnsonville</h2>
+
+      <div className={style.textContainer}>
+        <h3 className={style.text}>Made for Hipsters!</h3>
       </div>
+      
+      {
+        links
+      }
 
     </nav>
   )
