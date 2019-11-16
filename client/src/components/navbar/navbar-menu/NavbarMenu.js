0deea057bf6953faebbe828bf01f4e266739779f
mko5118 <minpko@gmail.com>
2019-11-16 17:38:06 -0500

    Update NavbarMenu to contain links to navigate around site when in mobile

--- client/src/components/navbar/navbar-menu/NavbarMenu.js
+++ client/src/components/navbar/navbar-menu/NavbarMenu.js
@@ -1,12 +1,21 @@
 import React, { } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { logoutUser } from '../../../redux/auth/auth.actions';
 
 import style from './navbar-menu.module.scss';
 
 // *************************** NAVBAR MENU COMPONENT *************************** //
-const NavbarMenu = ({ toggleMenu, setToggleMenu }) => {
-  // 'toggleMenu' & 'setToggleMenu' passed down as props from NavbarMobile
+const NavbarMenu = ({ toggleMenu, setToggleMenu, isAuthenticated, logoutUser }) => {
+  // 'toggleMenu' & 'setToggleMenu' passed down as props from NavbarMobile to allow for closing of menu on click
   const onClick = () => {
     setToggleMenu(!toggleMenu);
+  };
+
+  const onLogout = () => {
+    logoutUser();
+    window.location.reload();
   };
 
   return (
@@ -15,11 +24,74 @@ const NavbarMenu = ({ toggleMenu, setToggleMenu }) => {
       <div onClick={onClick} className={style.menuFiller} />
 
       <div className={style.menu}>
-        NAVBAR MENU
+        <div className={style.siteLinks}>
+          <ul className={style.links}>
+            <li>
+              <Link to='/' className={style.link}>Men's</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Women's</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Exclusives</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Forum</Link>
+            </li>
+            <br />
+            <li>
+              {
+                isAuthenticated 
+                  ? <p onClick={onLogout} className={style.link}>Log Out</p> 
+                  : <Link to='/account' className={style.link}>Log In</Link>
+              }
+            </li>
+          </ul>
+        </div>
+
+        <div className={style.footerLinks}>
+          <ul className={style.links}>
+            <li>
+              <Link to='/' className={style.link}>Returns & Exchanges</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Terms & Conditions</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>About Us</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Contact Us</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Instagram</Link>
+            </li>
+            <br />
+            <li>
+              <Link to='/' className={style.link}>Twitter</Link>
+            </li>
+          </ul>
+        </div>
       </div>
 
     </div>
   )
 };
 
-export default NavbarMenu;
+// REDUX
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logoutUser: () => dispatch(logoutUser()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavbarMenu);
