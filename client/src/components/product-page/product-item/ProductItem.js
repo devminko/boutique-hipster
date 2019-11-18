c43084807fb41f35eb7988df801e6b6642134428
mko5118 <minpko@gmail.com>
2019-11-17 21:11:15 -0500

    Update ProductItem with product information and update admin redux file

--- client/src/components/product-page/product-item/ProductItem.js
+++ client/src/components/product-page/product-item/ProductItem.js
@@ -1,4 +1,6 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
+import { Gallery, GalleryImage } from 'react-gesture-gallery';
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 import Button from '../../../components-ui/button/Button';
 
@@ -7,20 +9,54 @@ import style from './product-item.module.scss';
 // *************************** PRODUCT ITEM COMPONENT *************************** //
 const ProductItem = ({ exclusive }) => {
   // 'exclusive' passed as prop from ProductPage to render specific product details
-  const { id, item_name, item_color, item_price, item_images, item_url } = exclusive;
+  const { id, item_name, item_color, item_price, item_description, item_info, item_images, item_url } = exclusive;
+
+  const [ index, setIndex ] = useState(0)
 
   return (
     <div className={style.productItem}>
-      <img src={item_images[0]} alt={`${item_name}_${item_color}_${id}`} className={style.image} />
-      <img src={item_images[1]} alt={`${item_name}_${item_color}_${id}`} className={style.image} />
-      {
-        item_images[2] && <img src={item_images[2]} alt={`${item_name}_${item_color}_${id}`} className={style.image} />
-      }
-      <p>{item_url}</p>
-      <p>{id}</p>
-      <p>{item_name}</p>
-      <p>{item_color}</p>
-      <p>${item_price}.00</p>
+      <div className={style.imageContainer}>
+        <Gallery
+          className={style.galleryContainer}
+          style={{}}
+          index={index}
+          onRequestChange={i => {
+            setIndex(i);
+          }}
+          enableIndicators={false}
+          enableControls={false}
+        >
+          {item_images.map(image => (
+            <GalleryImage
+              key={`${item_name}_${item_color}_${id}`}
+              objectFit='cover' 
+              src={image}
+            />
+          ))}
+        </Gallery>
+        <span className={style.imageIndex}>
+          <FaArrowLeft className={style.icon} />
+          {index + 1} of {item_images.length}
+          <FaArrowRight className={style.icon} />
+        </span>
+      </div>
+
+      <div className={style.productMain}>
+        <p className={style.productName}>{item_name}</p>
+        <p className={style.productPrice}>${item_price}.00</p>
+      </div>
+
+        <p>{item_color}</p>
+        <p>{item_url}</p>
+        <p>{id}</p>
+        <p>{item_description}</p>
+      
+      <div className={style.productTertiary}>
+        <p className={style.productInfo}>Product Info</p>
+        <p>Fabric: {item_info[0]}</p>
+        <p>Fit: {item_info[1]}</p>
+      </div>
+
     </div>
   )
 };
--- client/src/redux/admin/admin.reducer.js
+++ client/src/redux/admin/admin.reducer.js
@@ -6,6 +6,11 @@ const INITIAL_STATE = {
       'item_name': 'Balaclava',
       'item_color': 'Sand',
       'item_price': 100,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/BALACLAVA_SAND.png',
         '../../assets/images/BALACLAVA_SAND_1.jpg',
@@ -20,6 +25,11 @@ const INITIAL_STATE = {
       'item_name': 'Balaclava',
       'item_color': 'Crimson',
       'item_price': 100,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/BALACLAVA_CRIMSON.png',
         '../../assets/images/BALACLAVA_CRIMSON_1.jpg',
@@ -34,6 +44,11 @@ const INITIAL_STATE = {
       'item_name': 'Balaclava',
       'item_color': 'Smoke',
       'item_price': 100,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/BALACLAVA_SMOKE.png',
         '../../assets/images/BALACLAVA_SMOKE_1.jpg',
@@ -48,6 +63,11 @@ const INITIAL_STATE = {
       'item_name': 'Balaclava',
       'item_color': 'Onyx',
       'item_price': 100,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/BALACLAVA_ONYX.png',
         '../../assets/images/BALACLAVA_ONYX_1.jpg',
@@ -62,6 +82,11 @@ const INITIAL_STATE = {
       'item_name': 'Balaclava',
       'item_color': 'Croco',
       'item_price': 100,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/BALACLAVA_CROCO.png',
         '../../assets/images/BALACLAVA_CROCO_1.jpg',
@@ -72,9 +97,14 @@ const INITIAL_STATE = {
     },
     {
       'id': 6,
-      'item_name': 'Felted_Scarf',
+      'item_name': 'Felted Scarf',
       'item_color': 'Midnight / Crimson',
       'item_price': 150,
+      'item_description': 'Brutally fashionable piece! This wearable item will keep you warm during extremely cold winters. Perfect to enter those Alps. Limited edition. Only 40 pieces have been produced.',
+      'item_info': [
+        '30% cashmere/ 70% merino wool',
+        'Perfect fitted, can be worn as a normal beanie as well, when worn as balaclava provides extreme comfort.'
+      ],
       'item_images': [
         '../../assets/images/FILTED_SCARF_MIDNIGHT_CRIMSON.png',
         '../../assets/images/FILTED_SCARF_MIDNIGHT_CRIMSON_1.jpg',
@@ -83,7 +113,8 @@ const INITIAL_STATE = {
       'item_url': '/product/felted_scarf/6',
       'onSale': false,
     },
-  ]
+  ],
+  newsletter: [],
 };
 
 export const adminReducer = (state = INITIAL_STATE, action) => {
