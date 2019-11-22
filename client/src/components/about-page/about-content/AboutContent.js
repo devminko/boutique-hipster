7f9a3cd8c53117204c57cd1aec6be3d302bbbcc0
mko5118 <minpko@gmail.com>
2019-11-22 13:35:30 -0500

    Update AboutContent component styling for media responsiveness

--- client/src/components/about-page/about-content/AboutContent.js
+++ client/src/components/about-page/about-content/AboutContent.js
@@ -6,18 +6,24 @@ import style from './about-content.module.scss';
 const AboutContent = () => {
   return (
     <div className={style.aboutContent}>
-      <h3 className={style.title}>
-        The Process of 
-        <br />
-        This 
-        <br />
-        to 
-        <br />
-        That
-      </h3>
-      <p>
-        All products are knitted on traditional machineries. The technique used to make these products has been passed down through generations. After the knitting process, the beanies are washed, pressed and checked thoroughly.
-      </p>
+
+      {/* HEADER SECTION */}
+      <div className={style.header}>
+        <h3 className={style.title}>The Process </h3>
+        <div className={style.imageContainer}>
+          <img src='../../../assets/images/ABOUT_IMAGE_2.png' alt='Fibers picture' className={style.image} />
+          <span className={style.title}>To</span>
+          <img src='../../../assets/images/ABOUT_IMAGE_3.png' alt='Fibers picture' className={style.image} />
+        </div>
+      </div>
+
+      {/* SUBHEADER SECTION */}
+      <div className={style.subheader}>
+        <p className={style.subtitle}>
+          All products are knitted on traditional machineries. The technique used to make these products has been passed down through generations. After the knitting process, the beanies are washed, pressed and checked thoroughly.
+        </p>
+      </div>
+
     </div>
   )
 };
