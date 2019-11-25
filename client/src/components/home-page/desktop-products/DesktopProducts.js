bbf39019a6694a0bfd02ec49282d39e2bb4bd609
mko5118 <minpko@gmail.com>
2019-11-25 18:02:10 -0500

    Add DesktopProduct component and update DesktopSection component

---
+++ client/src/components/home-page/desktop-products/DesktopProducts.js
@@ -0,0 +1,34 @@
+import React, { } from 'react';
+import { Link } from 'react-router-dom';
+
+import style from './desktop-products.module.scss';
+
+// *************************** DESKTOP PRODUCTS COMPONENT (HOME PAGE) *************************** //
+const DesktopProducts = () => {
+  return (
+    <div className={style.desktopProducts}>
+      
+      <div className={style.productItem}>
+        <Link to='/beanies'>
+          <img src='../../../assets/images/BEANIE_SAND_1.png' alt='Beanie Link' className={style.image} />
+          <h3 className={style.productLink}>Beanies</h3>
+        </Link>
+      </div>
+      <div className={style.productItem}>
+        <Link to='/gloves'>
+          <img src='../../../assets/images/GLOVES_ONYX_1.png' alt='Gloves Link' className={style.image} />
+          <h3 className={style.productLink}>Gloves</h3>
+        </Link>
+      </div>
+      <div className={style.productItem}>
+        <Link to='/exclusives'>
+          <img src='../../../assets/images/FELTED_SCARF_ROYAL_SMOKE.png' alt='Exclusives Link' className={style.image} />
+          <h3 className={style.productLink}>Exclusives</h3>
+        </Link>
+      </div>
+
+    </div>
+  )
+};
+
+export default DesktopProducts;
--- client/src/components/home-page/desktop-sections/DesktopSections.js
+++ client/src/components/home-page/desktop-sections/DesktopSections.js
@@ -1,29 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { } from 'react';
 import { withRouter } from 'react-router-dom';
+
+import DesktopProducts from '../desktop-products/DesktopProducts';
 
 import style from './desktop-sections.module.scss';
 
 // *************************** HOME SECTIONS COMPONENT *************************** //
-const HomeSections = ({ history }) => {
+const DesktopSections = ({ history }) => {
   return (
-    <Fragment>
-      <div className={style.desktopSection}>
-        <h2 onClick={() => history.push('/beanies')} className={style.desktopTitle}>Beanies</h2>
+    <div className={style.desktopSections}>
+
+      {/* TOP SECTION */}
+      {/* <div className={style.topSection}>
+        <div className={style.desktopSection}>
+          <h2 onClick={() => history.push('/gloves')} className={style.desktopTitle}>Gloves</h2>
+        </div>
+
+        <div className={style.desktopSection}>
+          <h2 onClick={() => history.push('/beanies')} className={style.desktopTitle}>Beanies</h2>
+        </div>
+      </div> */}
+
+      {/* PRODUCTS SECTION */}
+      <div className={style.productsSection}>
+        <DesktopProducts />
       </div>
 
-      <div className={style.desktopSection}>
-        <h2 onClick={() => history.push('/gloves')} className={style.desktopTitle}>Gloves</h2>
-      </div>
-
-      <div className={style.desktopSection}>
+      {/* <div className={style.desktopSection}>
         <h2 onClick={() => history.push('/exclusives')} className={style.desktopTitle}>Exclusives</h2>
       </div>
 
       <div className={style.desktopSection}>
         <h2 onClick={() => history.push('/account')} className={style.desktopTitle}>Sign Up</h2>
-      </div>
-    </Fragment>
+      </div> */}
+
+    </div>
   )
 };
 
-export default withRouter(HomeSections);
+export default withRouter(DesktopSections);
