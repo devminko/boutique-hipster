74bd542443b10aa3e6f50ee5e97f042aad395fe9
mko5118 <minpko@gmail.com>
2019-12-04 09:30:04 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,5 +16,16 @@ database.sync({ force: false })   // set to true to drop table on server start
   .then(() => console.log('Defined models synced to DB'))
   .catch(err => console.error(`Unable to sync database: ${err}`));
 
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  // serve the built index.js
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on Port: ${PORT}. Currently running in ${process.env.NODE_ENV} mode.`));
